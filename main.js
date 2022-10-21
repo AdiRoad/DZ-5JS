2de99a1a3c9fb0b8400b84a18e2cc07ec05b1d9e
ADI_ROAD7 <adiroad946@gmail.com>
2022-10-21 04:25:05 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,35 @@
+// ! 1 Задача !
+
+let a;
+if (a > 0) {
+  let ggg = function() {
+    console.log('!');
+  }
+} else {
+  let ggg = function() {
+    console.log('!!');
+  }
+} 
+
+// Задача 2
+
+// var arr = [];
+
+// for(i =1; i <= 10;i++) {
+//   arr[i] = 'x';
+// }
+// console.log(arr);
+ 
+// Задача 3
+
+// function counter() {
+//   let i = 1;
+//   return function() {return i++};
+// }
+
+// let func = counter();
+
+// console.log(func());
+// console.log(func());
+// console.log(func());
+// console.log(func());
